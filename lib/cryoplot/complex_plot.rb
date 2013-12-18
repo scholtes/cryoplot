8756255abb3bf6555597ea5f86278a89cfd16f29
scholtes <scholtes.garrett@gmail.com>
2013-12-18 16:47:48 -0500

    Fix option selecting and added contour basis

--- lib/cryoplot/complex_plot.rb
+++ lib/cryoplot/complex_plot.rb
@@ -5,8 +5,13 @@ module Cryoplot # :nodoc:
 	# Cryoplot::ComplexPlot represents an individual complex-valued plot.
 	class ComplexPlot < ChunkyPNG::Image
 
+		# Specify a domain colored plot
 		DOMAIN_COLORED_PLOT		= 0x1
+		# Specify a contour map plot
 		COMPLEX_CONTOUR_PLOT	= 0x2
+
+		# Default number of contour lines in either dimension.
+		DLINES = 10
 
 		# The number of functions on this plot.  Starts at 0, increments each time plot is called.
 		attr_reader :size
@@ -20,6 +25,9 @@ module Cryoplot # :nodoc:
 
 		# Size of margins, in pixels, around the graph.
 		attr_reader :top_margin, :bottom_margin, :left_margin, :right_margin
+
+		# Background color for margins and contour maps.
+		attr_reader :bg_color
 
 		# Initializes a new instance of Cryoplot::ComplexPlot
 		# @param width [Integer] The width of the plot area, in pixels (excluding margins)
@@ -35,7 +43,7 @@ module Cryoplot # :nodoc:
 		# @param bg_color [ChunkyPNG::Color] Default background color.
 		def initialize(width, height, min_r, max_r, min_i, max_i,
 				top_margin=0, bottom_margin=0, left_margin=0, right_margin=0,
-				bg_color = ChunkyPNG::Color::PREDEFINED_COLORS[:whitesmoke])
+				bg_color = ChunkyPNG::Color::PREDEFINED_COLORS[:whitesmoke]+0xFF)
 			super(width+left_margin+right_margin, height+top_margin+bottom_margin, bg_color)
 			@width, @height = width, height
 			@min_r, @max_r = min_r, max_r
@@ -48,17 +56,23 @@ module Cryoplot # :nodoc:
 
 		# Draws a plot of a complex-valued function.  Multiple functions may be graphed on one ComplexPlot instance.
 		# However, only one domain-colored plot can be drawn and it must come first, or it will draw over other plots.
+		# Parameters must be referenced by key.
 		# @param type [Integer] A constant reffering to the plot type, either DOMAIN_COLORED_PLOT or COMPLEX_CONTOUR_PLOT
-		# @param type color [ChunkyPNG::Color] If contour plot, the color of the contours.  If domain-colored, color of out-of-domain values.
+		# @param color [ChunkyPNG::Color] If contour plot, the color of the contours.  If domain-colored, color of out-of-domain values.
+		# @param vert [Integer] Number of vertical lines in the un-transformed map (if creating a contour plot).  Values must be >= 2
+		# @param horz [Integer] Number of horizontal lines in the un-transformed map.  >= 2
 		# :yields: z [Complex] Iterates through the domain of the plot.  The return value of each iteration in the block will be plotted at a pixel.
-		def plot(type=DOMAIN_COLORED_PLOT, color=ChunkyPNG::Color::BLACK, &block)
-			if type == DOMAIN_COLORED_PLOT and @size > 0
+		def plot(opts = {}, &block)
+			default_opts = {:type => DOMAIN_COLORED_PLOT, :color => ChunkyPNG::Color::BLACK,
+				:vert => DLINES, :horz => DLINES}
+			opts = default_opts.merge(opts)
+			if opts[:type] == DOMAIN_COLORED_PLOT and @size > 0
 				warn "Warning in #{self.class}::plot: DOMAIN_COLORED_PLOT will overdraw existing #{@size} plots.  Draw 1st for correct behavior."
 			end
-			case type
-				when DOMAIN_COLORED_PLOT then plotDCPlot(color, &block)
-				when COMPLEX_CONTOUR_PLOT then plotCCPlot(color, &block)
-				else raise RangeError, "Invalid plot type #{type}."
+			case opts[:type]
+				when DOMAIN_COLORED_PLOT then plotDCPlot(opts, &block)
+				when COMPLEX_CONTOUR_PLOT then plotCCPlot(opts, &block)
+				else raise RangeError, "Invalid plot type #{opts[:type]}."
 			end
 			@size += 1
 		end
@@ -68,16 +82,16 @@ module Cryoplot # :nodoc:
 
 		# TODO: Finish other private methods
 		# Like this: http://en.wikipedia.org/wiki/Domain_coloring
-		def plotDCPlot(color)
+		def plotDCPlot(opts)
 			for pixel_x in @left_margin..(@width+@left_margin-1)
 				for pixel_y in @top_margin..(@top_margin+@height-1)
 					z = pixelToComplex(pixel_x, pixel_y)
 					begin
 						fz = yield(z)
+						self[pixel_x, pixel_y] = complexToColor(fz)
 					rescue
-						fz = Complex(0.0, 0.0)
+						self[pixel_x, pixel_y] = opts[:color]
 					end
-					self[pixel_x, pixel_y] = complexToColor(fz)
 				end
 			end 
 		end
@@ -85,13 +99,35 @@ module Cryoplot # :nodoc:
 		# TODO: Make this actually do stuff
 		# Like this: http://en.wikipedia.org/wiki/Conformal_map
 		# Described here under "Using conformal maps": http://www.pacifict.com/ComplexFunctions.html
-		def plotCCPlot(color)
-			yield Complex(0,0) # This does nothing, stops from throwing exception.  Remove
+		def plotCCPlot(opts)
+			verticals = linspace(@left_margin, opts[:vert], @left_margin+@width-1)
+			horizontals = linspace(@top_margin, opts[:horz], @top_margin+@height-1)
+			for pixel_x in verticals
+				old_p = nil
+				for pixel_y in @top_margin..(@top_margin+@height-1)
+					z = pixelToComplex(pixel_x, pixel_y)
+					begin
+						fz = yield(z)
+						new_p = complexToPixel(fz)
+						 if !old_p.nil? and in_range?(old_p) and in_range?(new_p)
+							self.line(old_p[0],old_p[1],new_p[0],new_p[1],0x000000ff)
+						end
+						old_p = Array.new(new_p)
+					rescue
+						old_p = nil
+					end
+				end
+			end
 		end
 
 		def pixelToComplex(px, py)
 			Complex((px-@left_margin)*(@max_r-@min_r)/@width.to_f+@min_r,
 					(py-@top_margin)*(@min_i-@max_i)/@height.to_f+@max_i)
+		end
+
+		def complexToPixel(z)
+			[@width*(z.real-@min_r)/(@max_r-@min_r) + @left_margin,
+			@height*(z.imag-@max_i)/(@min_i-@max_i) + @top_margin]
 		end
 
 		def complexToColor(z)
@@ -126,7 +162,20 @@ module Cryoplot # :nodoc:
 			return ChunkyPNG::Color.rgb((255*r).to_i, (255*g).to_i, (255*b).to_i)
 		end
 
-		private :plotDCPlot, :plotCCPlot, :pixelToComplex, :complexToColor
+		# Integer linspace from x to y, with size n
+		def linspace(x, n, y)
+			a = []
+			for i in 0..(n-1)
+				a[i] = x*(n-1-i)/(n-1) + y*i/(n-1)
+			end
+			return a
+		end
+
+		def in_range?(pix)
+			pix[0].between?(@left_margin, @left_margin+@width-1) and pix[1].between?(@top_margin, @top_margin+@height-1)
+		end
+
+		private :plotDCPlot, :plotCCPlot, :pixelToComplex, :complexToColor, :complexToPixel, :linspace, :in_range?
 
 	end
 
--- test/test_cryoplot.rb
+++ test/test_cryoplot.rb
@@ -1,12 +1,29 @@
 require 'test/unit'
 require 'cryoplot'
+require 'cmath'
 
 class CryoplotTest < Test::Unit::TestCase
+
+	TEST_PATH = File.dirname(File.expand_path(__FILE__))
 
 	def test_constructor
 		plot1 = Cryoplot::ComplexPlot.new(16, 12, -4.0, 4.0, -3.0, 3.0)
 		assert_equal plot1.width, 16
 		assert_equal plot1.height, 12
+	end
+
+	def test_simple_domain_colored
+		plot = Cryoplot::ComplexPlot.new(32, 24, -4.0, 4.0, -3.0, 3.0)
+		plot.plot { |z| CMath.sin(z) }
+		plot.save(TEST_PATH+'/test_simple_domain_colored.png')
+		assert true
+	end
+
+	def test_simple_contour_plot
+		plot = Cryoplot::ComplexPlot.new(400, 300, -4.0, 4.0, -3.0, 3.0)
+		plot.plot(:type => Cryoplot::ComplexPlot::COMPLEX_CONTOUR_PLOT, :horz => 9, :vert => 7) { |z| CMath.sin(z) }
+		plot.save(TEST_PATH+'/test_simple_contour_plot.png')
+		assert true
 	end
 
 end
